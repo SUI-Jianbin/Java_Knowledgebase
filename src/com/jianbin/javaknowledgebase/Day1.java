0d41cca5407cb980d2e10c211ac86506d581f70e
SUI-Jianbin <suijianbin@gmail.com>
2021-08-24 22:53:38 +0800

    This, Constructor, Static

--- src/com/jianbin/javaknowledgebase/Day1.java
+++ src/com/jianbin/javaknowledgebase/Day1.java
@@ -9,6 +9,19 @@ public class Day1 {
         student1.StuName = "Jianbin";
         student1.SayHello();
         student1.Study();
+        Student.CountStu();
+        Student.CountStu();
+
+        Student student2 = new Student("Paul",26,'M');
+        Student.CountStu();
+        student2.Study();
+        student2.SayHello();
+        Course course1 = new Course();
+        course1.cID = 01;
+        course1.cName = "English";
+        course1.cTeacher = "James";
+        System.out.println(course1.cName);
+        System.gc();
     }
 
 }
--- src/com/jianbin/javaknowledgebase/Student.java
+++ src/com/jianbin/javaknowledgebase/Student.java
@@ -1,11 +1,26 @@
 package com.jianbin.javaknowledgebase;
 
 public class Student {
+    static int count;
     public String StuName;
     public int StuId;
     public int StuAge;
     public char StuGender;
 
+    public static void CountStu(){
+        count++;
+        System.out.println(count);
+    }
+
+    public Student(){
+
+    }
+    public Student (String name, int age, char gender){
+        this();
+        this.StuName = name;
+        this.StuAge = age;
+        this.StuGender = gender;
+    }
     public void Study (){
         System.out.println("Hello, I am learning Java now.");
     }
